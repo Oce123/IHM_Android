f30a76276a96e12fb80cfc4dae2b4d8902889ccb
Louise <louise.faillant@edu.esiee.fr>
2022-11-13 17:43:06 +0100

    mise en page page param

--- app/src/main/java/com/example/ihm/SettingsActivity.java
+++ app/src/main/java/com/example/ihm/SettingsActivity.java
@@ -1,8 +1,13 @@
 package com.example.ihm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.*;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,4 +16,7 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+
+
 }
