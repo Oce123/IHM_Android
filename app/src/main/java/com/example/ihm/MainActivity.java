9fa2f9b7ff9d50f92b026b93e4c4d118b6a0466f
Oce123 <oceane.champagne@edu.esiee.fr>
2022-11-23 13:15:08 +0100

    Correction erreur main

--- app/src/main/java/com/example/ihm/MainActivity.java
+++ app/src/main/java/com/example/ihm/MainActivity.java
@@ -32,7 +32,6 @@ import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
     MainFragment mainFragment;
-    private CommentsDataSource datasource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,44 +57,6 @@ public class MainActivity extends AppCompatActivity {
         Intent redirect = new Intent(this, TableActivity.class);
         startActivity(redirect);
     }
-
-    // Sera appelée par l'attribut onClick
-    // des boutons déclarés dans main.xml
-    public void onClick(View view) {
-        @SuppressWarnings("unchecked")
-        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>) getListAdapter();
-        Comment comment = null;
-        switch (view.getId()) {
-            case R.id.add:
-                String[] comments = new String[]{"Cool", "Very nice", "Hate it"};
-                int nextInt = new Random().nextInt(3);
-                // enregistrer le nouveau commentaire dans la base de données
-                comment = datasource.createComment(comments[nextInt]);
-                adapter.add(comment);
-                break;
-            case R.id.delete:
-                if (getListAdapter().getCount() > 0) {
-                    comment = (Comment) getListAdapter().getItem(0);
-                    datasource.deleteComment(comment);
-                    adapter.remove(comment);
-                }
-                break;
-        }
-        adapter.notifyDataSetChanged();
-    }
-
-    @Override
-    protected void onResume() {
-        datasource.open();
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        datasource.close();
-        super.onPause();
-    }
-
 
     private InputStream HttpConnection(){
         InputStream in = null;
